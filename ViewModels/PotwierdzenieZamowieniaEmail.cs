1ad95409f0180e12fcff03396f2aa0565ea5c13a
Ambioda <ambioda12@interia.pl>
2024-01-08 11:34:32 +0100

    Dodanie powiadomień email dotyczących potwierdzenia zamówienia oraz zmiany statusu zamówienia.

---
+++ ViewModels/PotwierdzenieZamowieniaEmail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Hurtownia.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Postal;
+
+namespace Hurtownia.ViewModels
+{
+    public class PotwierdzenieZamowieniaEmail : Email
+    {
+        public string To { get; set; }
+            public string From { get; set; }
+        public decimal Wartosc { get; set; }
+        public int NumerZamowienia { get; set; }
+        public string Komentarz { get; set; }
+        public List<PozycjaZamowienia> PozycjeZamowienia { get; set; }
+    }
+}
---
+++ ViewModels/PotwierdzenieZmianyStatusu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Postal;
+using Hurtownia.Models;
+
+namespace Hurtownia.ViewModels
+{
+    public class PotwierdzenieZmianyStatusu : Email
+    {
+
+            public string To { get; set; }
+            public string From { get; set; }
+            public int NumerZamowienia { get; set; }
+        public int StanZamowienia { get; set; }
+        }
+    }
